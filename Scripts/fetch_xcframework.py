0ce7a865b5b53701fcfde6a9d0f328d93045a27c
a dinosaur <dino@a-dinosaur.com>
2025-05-04 02:20:38 +1000

    Script to automate fetching & preparing SDL3.xcframework

---
+++ Scripts/fetch_xcframework.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: (C) 2025 Gay Pizza Specifications <gay.pizza>
+# SPDX-License-Identifier: Zlib OR 0BSD
+
+import shutil
+import sys
+from pathlib import Path
+import urllib.request
+import urllib.error
+import tempfile
+import subprocess
+from generate_sdl3_modulemaps import generate_shims_modulemaps
+
+
+def build(release_ver: str):
+  # Find path to target XCFramework
+  root = Path(sys.argv[0]).resolve().parent.parent
+  xcframework = root / "Frameworks/SDL3.xcframework"
+
+  # Download specified SDL release to a temporary DMG disk image
+  ver = release_ver
+  with tempfile.NamedTemporaryFile(suffix=".dmg") as fp:
+    try:
+      url = f"https://github.com/libsdl-org/SDL/releases/download/release-{ver}/SDL3-{ver}.dmg"
+      print("Downloading:", url, file=sys.stderr)
+      fp.write(urllib.request.urlopen(url).read())
+    except urllib.error.URLError as e:
+      exit(e)
+
+    # Mount DMG at '/Volumes/SDL3'
+    subprocess.run(["hdiutil", "attach", fp.name], check=True)
+
+    try:
+      # Remove old xcframework
+      if xcframework.is_dir():
+        shutil.rmtree(xcframework)
+
+      # Copy xcframework from DMG to destination
+      shutil.copytree("/Volumes/SDL3/SDL3.xcframework", xcframework, symlinks=True)
+    finally:
+      # Unmount disk image
+      subprocess.run(["hdiutil", "detach", "/Volumes/SDL3"], check=True)
+
+  # Generate shims and modulemaps for each available library
+  generate_shims_modulemaps(xcframework)
+
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print(f"Usage: ./{Path(sys.argv[0]).name} <version>", file=sys.stderr)
+  else:
+    build(sys.argv[1])
--- Scripts/generate_sdl3_modulemaps.py
+++ Scripts/generate_sdl3_modulemaps.py
@@ -2,23 +2,16 @@
 # SPDX-FileCopyrightText: (C) 2025 Gay Pizza Specifications <gay.pizza>
 # SPDX-License-Identifier: Zlib OR 0BSD
 
-import re
 import sys
 from pathlib import Path
 import plistlib
 from generate_sdl3_shim import generate_shim
 
 
-def main():
-  # Find path to target XCFramework
-  root = Path(sys.argv[0]).resolve().parent.parent
-  xcframework = root / "Frameworks/SDL3.xcframework"
-
+def generate_shims_modulemaps(xcframework: Path):
   # Read XCFramework property list
   with xcframework.joinpath("Info.plist").open("rb") as f:
     plist = plistlib.load(f)
-
-  sdl_uint64c_pattern = re.compile(r"#define\s+(\w+)\s+SDL_UINT64_C\((\w+)\)\s+/\*(.+)\*/")
 
   for a in plist["AvailableLibraries"]:
     # Get framework paths
@@ -37,8 +30,9 @@ def main():
     modules.mkdir(exist_ok=True)
     if str(modules.relative_to(framework)).startswith("Versions"):
       symlink = framework.joinpath("Modules")
-      if not framework.joinpath("Modules").is_symlink():
-        symlink.symlink_to("Versions/Current/Modules")
+      if symlink.is_symlink():
+        symlink.unlink()
+      symlink.symlink_to("Versions/Current/Modules")
 
     # Generate module.modulemap for framework
     with modules.joinpath("module.modulemap").open("w") as map:
@@ -50,4 +44,6 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  # Find path to target XCFramework
+  root = Path(sys.argv[0]).resolve().parent.parent
+  generate_shims_modulemaps(root / "Frameworks/SDL3.xcframework")
