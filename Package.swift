d0e5abf87589c2969d3539e622974ce9a60ae1b1
Alex Zenla <alex@edera.dev>
2025-05-11 02:03:24 -0700

    mark iOS, tvOS, and Mac Catalyst as supported platforms

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "SDL3Swift",
-  platforms: [ .macOS(.v10_13), .iOS(.v12), .tvOS(.v12) ],
+  platforms: [ .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .macCatalyst(.v13) ],
   products: [
     .library(name: "SDLSwift", targets: [ "SDLSwift" ]),
   ],
@@ -14,7 +14,7 @@ let package = Package(
       dependencies: [
         .target(
           name: "SDLFramework",
-          condition: .when(platforms: [ .macOS ])),
+          condition: .when(platforms: [ .macOS, .iOS, .tvOS, .macCatalyst ])),
         .target(
           name: "CSDL3",
           condition: .when(platforms: [ .linux, .windows ])),
