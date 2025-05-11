7f5257f356342e73dabeba7c32c824f7be87fa09
Alex Zenla <alex@edera.dev>
2025-05-11 01:59:23 -0700

    fix import for iOS

--- Sources/SDLSwift/SDL3.swift
+++ Sources/SDLSwift/SDL3.swift
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: (C) 2025 Gay Pizza Specifications <gay.pizza>
 // SPDX-License-Identifier: Zlib OR 0BSD
 
-#if CMAKE_BUILD || os(macOS)
+#if CMAKE_BUILD || canImport(Darwin)
 @_exported import SDL3
 #else
 @_exported import CSDL3
