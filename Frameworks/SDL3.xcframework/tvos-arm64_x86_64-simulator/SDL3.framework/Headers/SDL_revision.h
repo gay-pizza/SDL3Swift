3446d17d4e5f527ef243fddc248b496f0d921b28
a dinosaur <dino@a-dinosaur.com>
2025-11-20 13:27:37 +1100

    Update bundled SDL3.xcframework to 3.2.26

--- Frameworks/SDL3.xcframework/tvos-arm64_x86_64-simulator/SDL3.framework/Headers/SDL_revision.h
+++ Frameworks/SDL3.xcframework/tvos-arm64_x86_64-simulator/SDL3.framework/Headers/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.26-0-gbadbf8da4 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5"
+#define SDL_REVISION "release-3.2.26-0-gbadbf8da4"
 #endif
 
 #endif /* SDL_revision_h_ */
